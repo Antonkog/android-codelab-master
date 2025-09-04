77f055c958164550ddb033db3de8935117a12a3d
Anton Kogan <contact@antonkogan.de>
2025-09-04 23:27:16 +0200

    Replace @WorkerThread with suspend function.

--- app/src/main/java/com/sap/codelab/core/data/Repository.kt
+++ app/src/main/java/com/sap/codelab/core/data/Repository.kt
@@ -1,6 +1,5 @@
 package com.sap.codelab.core.data
 
-import androidx.annotation.WorkerThread
 import com.sap.codelab.core.data.db.Database
 import com.sap.codelab.core.domain.IMemoRepository
 import com.sap.codelab.core.domain.Memo
@@ -10,17 +9,11 @@ import com.sap.codelab.core.domain.Memo
  */
 internal class Repository(private val database: Database) : IMemoRepository {
 
-    @WorkerThread
-    override fun saveMemo(memo: Memo) {
-        database.getMemoDao().insert(memo)
-    }
+    override suspend fun saveMemo(memo: Memo) = database.getMemoDao().insert(memo)
 
-    @WorkerThread
-    override fun getOpen(): List<Memo> = database.getMemoDao().getOpen()
+    override suspend fun getOpen(): List<Memo> = database.getMemoDao().getOpen()
 
-    @WorkerThread
-    override fun getAll(): List<Memo> = database.getMemoDao().getAll()
+    override suspend fun getAll(): List<Memo> = database.getMemoDao().getAll()
 
-    @WorkerThread
-    override fun getMemoById(id: Long): Memo = database.getMemoDao().getMemoById(id)
+    override suspend fun getMemoById(id: Long): Memo = database.getMemoDao().getMemoById(id)
 }
--- app/src/main/java/com/sap/codelab/core/domain/IMemoRepository.kt
+++ app/src/main/java/com/sap/codelab/core/domain/IMemoRepository.kt
@@ -9,20 +9,20 @@ interface IMemoRepository {
     /**
      * Saves the given memo to the database.
      */
-    fun saveMemo(memo: Memo)
+    suspend fun saveMemo(memo: Memo)
 
     /**
      * @return all memos currently in the database.
      */
-    fun getAll(): List<Memo>
+    suspend fun getAll(): List<Memo>
 
     /**
      * @return all memos currently in the database, except those that have been marked as "done".
      */
-    fun getOpen(): List<Memo>
+    suspend fun getOpen(): List<Memo>
 
     /**
      * @return the memo whose id matches the given id.
      */
-    fun getMemoById(id: Long): Memo
+    suspend fun getMemoById(id: Long): Memo
 }
